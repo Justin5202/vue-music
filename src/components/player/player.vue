dac0c2d58c84d934ce89358eaa2269e86666661d
justin5202 <386659643@qq.com>
2017-11-09 22:19:31 +0800

    music

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,23 +1,180 @@
 <template>
 	<div class="player" v-show="playList.length > 0">
-		<div class="normal-player" v-show="fullScreen">
-			
-		</div>
-		<div class="mini-player" v-show="!fullScreen">
-			
-		</div>
+		<transition name="normal"
+					@enter="enter"
+					@after-enter='afterEnter'
+					@leave="leave"
+					@after-leave="afterLeave"
+		>
+			<div class="normal-player" v-show="fullScreen">
+				<div class="background">
+					<img :src="currentSong.image" width="100%" height="100%">
+				</div>
+				<div class="top">
+					<div class="back" @click="back">
+						<i class="icon-back"></i>
+					</div>
+					<h1 class="title" v-html="currentSong.name"></h1>
+					<h2 class="subtitle" v-html="currentSong.singer"></h2>
+				</div>
+				<div class="middle">
+					<div class="middle-l">
+						<div class="cd-wrapper" ref="cdWrapper">
+							<div class="cd" :class="cdCls">
+								<img :src="currentSong.image" class="image">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom">
+					<div class="operators">
+						<div class="icon i-left">
+							<i class="icon-sequence"></i>
+						</div>
+						<div class="icon i-left">
+							<i class="icon-prev"></i>
+						</div>
+						<div class="icon i-center">
+							<i :class="playIcon" @click="togglePlaying"></i>
+						</div>
+						<div class="icon i-right">
+							<i class="icon-next"></i>
+						</div>
+						<div class="icon i-right">
+							<i class="icon icon-not-favorite"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</transition>
+		<transition name="mini">
+			<div class="mini-player" v-show="!fullScreen" @click="open">
+				<div class="icon">
+					<img :src="currentSong.image" :class="cdCls" width="40" height="40">
+				</div>
+				<div class="text">
+					<h2 class="name" v-html="currentSong.name"></h2>
+					<p class="desc" v-html="currentSong.singer"></p>
+				</div>
+				<div class="control">
+					<i :class="miniIcon" @click.stop="togglePlaying"></i>
+				</div>
+				<div class="control">
+					<i class="icon-playlist"></i>
+				</div>
+			</div>
+		</transition>
+		<audio :src="currentSong.url" ref="audio"></audio>
 	</div>
 </template>
 
 <script>
-	import {mapGetters} from 'vuex'
+	import {mapGetters, mapMutations} from 'vuex'
+	import animations from 'create-keyframe-animation'
+	import {prefixStyle}  from 'common/js/dom'
+
+	const transform = prefixStyle('transform')
 
 	export default {
 		computed: {
+			cdCls() {
+				return this.playing ? 'play' : 'play pause'
+			},
+			playIcon() {
+				return this.playing ? 'icon-pause' : 'icon-play'
+			},
+			miniIcon() {
+				return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+			},
 			...mapGetters([
 				'fullScreen',
-				'playList'
+				'playList',
+				'currentSong',
+				'playing'
 			])
+		},
+		methods: {
+			back() {
+				this.setFullScreen(false)
+			},
+			open() {
+				this.setFullScreen(true)
+			},
+			enter(el, done) {
+				const {x, y, scale} = this._getPosAndScale()
+
+				let animation = {
+					0: {
+						transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+					},
+					60: {
+						transform: `translate3d(0, 0, 0) scale(1.1)`
+					},
+					100: {
+						transform: `translate3d(0, 0, 0) scale(1)`
+					}
+				}
+				//注册
+				animations.registerAnimation({
+					name: 'move',
+					animation,
+					presets: {
+						duration: 400,
+						easing: 'linear'
+					}
+				})
+				//运行
+				animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+			},
+			afterEnter() {
+				animations.unregisterAnimation('move')
+				this.$refs.cdWrapper.style.animation = ''
+			},
+			leave(el, done) {
+				this.$refs.cdWrapper.style.transition = 'all 0.4s'
+				const {x, y, scale} = this._getPosAndScale()
+				this.$refs.cdWrapper.style[transform] = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+				this.$refs.cdWrapper.addEventListener('transitionend', done);
+			},
+			afterLeave() {
+				this.$refs.cdWrapper.style.transition = ''
+				this.$refs.cdWrapper.style[transform] = ''
+			},
+			togglePlaying() {
+				this.setPlayingState(!this.playing)
+			},
+			_getPosAndScale() {
+				const targetWidth = 40
+				const paddingLeft = 40
+				const paddingBottom = 30
+				const paddingTop = 80
+				const width = window.innerWidth * 0.8
+				const scale = targetWidth / width
+				const x = -(window.innerWidth / 2 - paddingLeft)
+				const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+				return {
+					x,
+					y,
+					scale
+				}
+			},
+			...mapMutations({
+				setFullScreen: 'SET_FULL_SCREEN',
+				setPlayingState: 'SET_PLAYING_STATE'
+			})
+		},
+		watch: {
+			currentSong() {
+				this.$nextTick(() => {
+					this.$refs.audio.play()
+				})
+			},
+			playing(newPlaying) {
+				const audio = this.$refs.audio
+				this.$nextTick(() => {
+					newPlaying ? audio.play() : audio.pause()
+				})
+			}
 		}
 	}
 </script>
